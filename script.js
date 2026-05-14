05b2b71b10a07a3280e4b87c7ec0b6aee0ce992c
almyrarf-ops <almyrarf@gmail.com>
2026-05-14 23:02:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-localStorage.clear();
-
 const container = document.querySelector('.container');
 const registerBtn = document.querySelector('.register-btn');
 const loginBtn = document.querySelector('.login-btn');
